7a95a115aa851d5338fb4a1326db07f88bc8cb87
citruscaviar <citruscaviar96@gmail.com>
2024-09-11 19:49:47 +0300

    online-book-store part 2 completed

--- src/main/java/com/onlinebookstore/store/repository/BookRepository.java
+++ src/main/java/com/onlinebookstore/store/repository/BookRepository.java
@@ -2,9 +2,12 @@ package com.onlinebookstore.store.repository;
 
 import com.onlinebookstore.store.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
     Book save(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 }
--- src/main/java/com/onlinebookstore/store/repository/BookRepositoryImpl.java
+++ src/main/java/com/onlinebookstore/store/repository/BookRepositoryImpl.java
@@ -2,47 +2,51 @@ package com.onlinebookstore.store.repository;
 
 import com.onlinebookstore.store.exceptions.DataProcessingException;
 import com.onlinebookstore.store.model.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public Book save(Book book) {
-        Transaction transaction = null;
-        Session session = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
             transaction.commit();
             return book;
-        } catch (Exception e) {
-            if (transaction != null) {
+        } catch (RuntimeException e) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             throw new DataProcessingException("Cannot add a book to the database", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Book", Book.class).list();
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             throw new DataProcessingException("Failed to find all Books", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Book book = entityManager.find(Book.class, id);
+            return Optional.ofNullable(book);
         }
     }
 }
